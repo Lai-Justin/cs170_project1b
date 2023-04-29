9a6399f9ae9832f3083b27295e872aef97ed146a
Sheldin Lau <sheldinlau@umail.ucsb.edu>
2023-04-29 12:47:26 -0700

    modified read write lock

--- p1b-skeleton/threads/rwlock.cc
+++ p1b-skeleton/threads/rwlock.cc
@@ -8,11 +8,60 @@
 //Your solution for  Task 3 
 //TODO
 
-RWLock::RWLock(){ }
+RWLock::RWLock(){
+    AR = 0;
+    WR = 0;
+    AW = 0;
+    WW = 0;
+    okToRead = Condition("okToRead");
+    okToWrite = Condition("okToWrite");
+    lock = Lock("lock");
+ }
 
-RWLock::~RWLock(){ }
+RWLock::~RWLock(){
+    okToRead.~Condition();
+    okToWrite.~Condition();
+    lock.~Condition();
+ }
 
-void RWLock::startRead(){ }
-void RWLock::doneRead(){ }
-void RWLock::startWrite(){ }
-void RWLock::doneWrite(){ }
+void RWLock::startRead(){
+    int status;
+    lock.Acquire();
+    while ((AW + WW) > 0) { // Is it safe to read?
+        WR++; // No. Writers exist
+        okToRead.Wait(lock);
+        WR--; // No longer waiting
+    }
+    AR++; // Now we are active!
+    lock.Release();
+ }
+void RWLock::doneRead(){
+    int status;
+    lock.Acquire();
+    AR--; // No longer active
+    if (AR == 0 && WW > 0) // No other active readers
+        okToWrite.Signal(lock); // Wake up one writer
+    lock.Release();
+ }
+void RWLock::startWrite(){
+    int status;
+    lock.Acquire();
+    while ((AW + AR) > 0) { // Is it safe to write?
+        WW++; // No. Active users exist
+        okToWrite.Wait(lock);
+        WW--; // No longer waiting
+    }
+    AW++; // Now we are active!
+    lock.Release();
+ }
+void RWLock::doneWrite(){
+    int status;
+    lock.Release();
+    AW--; // No longer active
+    if (WW > 0){ // Give priority to writers
+        okToWrite.Signal(lock); // Wake up one writer
+    } else if (WR > 0) { // Otherwise, wake reader
+        okToRead.Broadcast(lock); // Wake all readers
+    }
+    lock.Release()
+ }
--- p1b-skeleton/threads/rwlock.h
+++ p1b-skeleton/threads/rwlock.h
@@ -7,6 +7,14 @@ class RWLock{
 private:
       //Add  fields for  RWLock. Your solution to Task 3
       //TODO
+	  int AR;
+	  int WR;
+	  int AW;
+	  int WW;
+	  Condition okToRead;
+	  Condition okToWrite;
+	  Lock lock;
+	  
 public:
     	RWLock();
     	~RWLock();
